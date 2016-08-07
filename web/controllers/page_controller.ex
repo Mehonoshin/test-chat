79839f9edac01c85de2ab345c9b5c1a95a7ef45b
Stanislav Mekhonoshin <ejabberd@gmail.com>
2016-08-07 23:56:41 +0300

    Playing with redis

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Chat.PageController do
   use Chat.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    data = Chat.Redis.run_query(["keys", "*"])
+    render conn, "index.html", data: data
   end
 end
